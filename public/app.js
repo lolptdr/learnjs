3008e010e4ca104a8fc199fc9fce179e11974538
Joseph Tingsanchali <jtingsan@gmail.com>
2017-09-23 23:20:40 -0500

    Add unit test for views and parameterized the hash

---
+++ public/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var learnjs = {};
+
+learnjs.problemView = function(hash) {
+  return $('<div class="problem-view">').text('Coming soon!');
+}
+
+learnjs.showView = function(hash) {
+  var routes = {
+    '#problem': learnjs.problemView
+  };
+
+  var hashParts = hash.split('-');
+  var viewFn = routes[hashParts[0]];
+
+  if(viewFn) {
+    $('.view-container').empty().append(viewFn(hashParts[1]));
+  }
+}
